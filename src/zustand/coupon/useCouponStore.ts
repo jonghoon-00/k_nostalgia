3b8345dfb8d34c8e954694fd39a76e616eec3033
jonghoon <jonghoon7431@gmail.com>
2025-03-24 21:18:39 +0900

    Feat: 쿠폰 storage 할인 금액 state, 할인 금액 적용 객체 추가

--- src/zustand/coupon/useCouponStore.ts
+++ src/zustand/coupon/useCouponStore.ts
@@ -2,14 +2,23 @@ import { create } from "zustand";
 
 type State = {
   coupon: string | null;
+  discountAmount: number;
 }
 type Actions = {
   setCoupon: (coupon: string | null) => void;
 }
 
+const discountAmountMap:{[key: string]: number} = {
+  'https://kejbzqdwablccrontqrb.supabase.co/storage/v1/object/public/images/Coupon.png' : 2000,
+};
+
 const useCouponStore = create<State&Actions>((set) => ({
   coupon: null,
-  setCoupon: (coupon) => set({ coupon }),
+  discountAmount: 0,
+  setCoupon: (coupon) => {
+    const discountAmount = coupon ? discountAmountMap[coupon] || 0 : 0;
+    set({ coupon, discountAmount });
+  },
 }));
 
 export default useCouponStore;
