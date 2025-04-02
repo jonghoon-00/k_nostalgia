3f49a265823abaab203cfd3ea567b7c68be30395
jonghoon <jonghoon7431@gmail.com>
2025-04-02 21:06:43 +0900

    Feat: 결제 대행사 선택 컴포넌트 구현 완료

--- src/app/(pages)/(payment)/payment/components/PaymentMethodSelect.tsx
+++ src/app/(pages)/(payment)/payment/components/PaymentMethodSelect.tsx
@@ -1,28 +1,43 @@
 'use client';
 
+import RadioGroup from '@/components/ui/RadioGroup';
 import { usePaymentRequestStore } from '@/zustand/payment/usePaymentStore';
 
 const PaymentMethodSelect = () => {
-  const paymentMethods = ['토스페이', '카카오페이', '일반결제'];
   const { payMethod, setPayMethod } = usePaymentRequestStore();
+
+  const paymentOptions = [
+    { value: 'toss', label: '토스페이', icon: '/image/tossIcon.png' },
+    { value: 'kakao', label: '카카오페이', icon: '/image/kakaoPayIcon.png' },
+    { value: 'normal', label: '일반결제' }
+  ];
 
   return (
     <div className="bg-white p-4 flex flex-col gap-2 rounded-[12px] border-2 border-[#E0E0E0] mb-4">
       <h2 className="text-label-strong text-[18px] font-semibold">결제 수단</h2>
       <div className="">
-        {paymentMethods.map((method) => (
-          <label key={method} className="flex gap-1 items-center">
-            <input
-              type="radio"
-              name="payment"
-              value={method}
-              checked={payMethod === method}
-              onChange={() => setPayMethod(method)}
-              className="accent-primary-20 w-5"
-            />
-            <span className="text-gray-700">{method}</span>
-          </label>
-        ))}
+        <RadioGroup
+          name="payMethod"
+          options={paymentOptions.map((option) => ({
+            value: option.value,
+            label: (
+              <div className="flex items-center gap-2">
+                {option.icon && (
+                  <img
+                    src={option.icon}
+                    alt={option.label}
+                    width={17}
+                    height={17}
+                  />
+                )}
+                <span>{option.label}</span>
+              </div>
+            )
+          }))}
+          selectedValue={payMethod}
+          onChange={setPayMethod}
+          withDivider
+        />
       </div>
     </div>
   );
