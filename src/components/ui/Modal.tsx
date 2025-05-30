012f230f7a93eddf09b082ba3ac950ad214c0b77
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-05-30 17:35:50 +0900

    Fix: 모달 컴포넌트 수정(모바일 풀 화면 대응 추가)
    isFullOnMobile props를 true로 넘길 경우, 모바일에선 전체 화면, 데스크탑에선 모달로 보이도록 제작
    
    실제 적용하면서 스타일 수정 필요할 수 있음음

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,17 +1,35 @@
 // children 형태로 감싸서 사용 (<Modal>{내부 요소}</Modal>)
 
+import clsx from 'clsx';
 import React from 'react';
+import { BackButton } from '../icons/BackButton';
 
 interface ModalProps {
   isModalOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
-  modalHeader: React.ReactNode;
+
+  modalHeader?: React.ReactNode;
   className?: string;
   width?: string;
   height?: string;
+  isFullOnMobile?: boolean;
 }
 
+/**
+ *
+ * 모바일 - isFullOnMobile=true일 경우 전체화면 대응
+ *
+ * @property {boolean} isModalOpen - 모달 오픈 여부 state
+ * @property {() => void} onClose - 모달 닫기 function
+ * @property {React.ReactNode} children
+ * @property {React.ReactNode} modalHeader - 모달 헤더
+ * @property {string} [className] - 모달 컨테이너에 추가할 스타일(class)
+ * @property {string} [width] - 데스크탑 기준 모달 너비(px)
+ * @property {string} [height] - 데스크탑 기준 모달 높이(px)
+ * @property {boolean} [isFullOnMobile] - 모바일에서 전체 화면으로 표시 여부
+ *
+ */
 export const Modal: React.FC<ModalProps> = ({
   isModalOpen,
   onClose,
@@ -19,25 +37,50 @@ export const Modal: React.FC<ModalProps> = ({
   className,
   modalHeader,
   width,
-  height
+  height,
+  isFullOnMobile
 }) => {
   if (!isModalOpen) return null;
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[9999]"
+      className={clsx(
+        'fixed inset-0 z-[9999]',
+        'bg-black bg-opacity-50',
+        'flex items-center justify-center'
+      )}
+      // className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[9999]"
       onClick={onClose}
     >
       <div
-        className={`bg-white rounded-2xl ${className}`}
+        className={clsx(
+          'bg-white',
+          isFullOnMobile
+            ? 'rounded-none w-full h-full max-h-none'
+            : 'md:rounded-2xl',
+          className
+        )}
         style={{
-          width: width ? `${width}px` : 'auto',
-          height: height ? `${height}px` : 'auto', // 외부에서 높이 조정 가능하도록 추가
-          maxHeight: 'calc(100vh - 40px)' // 화면을 넘지 않도록 제한
+          width: isFullOnMobile ? undefined : width ? `${width}px` : 'auto',
+          height: isFullOnMobile ? undefined : height ? `${height}px` : 'auto',
+          maxHeight: isFullOnMobile ? undefined : 'calc(100vh - 40px)'
         }}
         onClick={(e) => e.stopPropagation()}
       >
-        {modalHeader}
+        {isFullOnMobile && (
+          <div
+            className={clsx(
+              'md:hidden flex justify-between items-center',
+              'px-4 py-3 border-b'
+            )}
+          >
+            <button onClick={onClose}>
+              <BackButton />
+            </button>
+            <div>{modalHeader}</div>
+          </div>
+        )}
+        {!isFullOnMobile && <div>{modalHeader}</div>}
         <div>{children}</div>
       </div>
     </div>
