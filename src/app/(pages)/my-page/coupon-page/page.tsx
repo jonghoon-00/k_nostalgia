f5dccf061675d7e88541ef76aa5174955e4d1692
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-06-16 01:10:25 +0900

    Fix: 마이페이지에서 쿠폰 이미지 로드 안되는 문제 수정
    + 안 쓰는 파일 제거

--- src/app/(pages)/my-page/coupon-page/page.tsx
+++ src/app/(pages)/my-page/coupon-page/page.tsx
@@ -1,4 +1,4 @@
-import { getCouponImageUrlList } from '@/app/api/coupon/getCouponList';
+import { getCouponImageUrlList } from '@/app/api/coupon/getCouponForServer';
 import Image from 'next/image';
 import CouponContents from './components/CouponContents';
 
@@ -10,7 +10,7 @@ const CouponPage = async () => {
       <div className="hidden md:mb-[48px] md:flex">
         <Image
           src="/image/Coupon_Tiger2.png"
-          alt="마이페이지 쿠폰호랑이 "
+          alt="마이페이지 쿠폰호랑이"
           width={141}
           height={88}
           className="w-[141px] h-[88px]"
