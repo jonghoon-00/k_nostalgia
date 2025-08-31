d5a1185f66aaabe30dd5bad606e0fd18d6f51391
jonghoon <152875407+jonghoon-00@users.noreply.github.com>
2025-08-31 14:34:49 +0900

    Design: 로그인 페이지 컨테이너 중앙 정렬

--- src/app/(pages)/log-in/_components/DesktopLayout.tsx
+++ src/app/(pages)/log-in/_components/DesktopLayout.tsx
@@ -1,30 +1,57 @@
-import React from 'react';
+import clsx from 'clsx';
 import Image from 'next/image';
 import LoginForm from './LoginForm';
-import TitleLogo from '@/components/icons/TitleLogo';
 
 const DesktopLoginLayout = () => {
+  const rootCls = clsx(
+    'hidden md:flex justify-center items-center',
+    'min-h-screen',
+    'bg-primary-20'
+  );
+
+  const cardCls = clsx(
+    'overflow-hidden bg-normal',
+    ' border rounded-[20px]',
+    'h-[870px] w-full',
+    'flex'
+  );
+
+  const leftPaneCls = clsx(
+    'w-1/2 bg-normal p-8',
+    'flex flex-col items-center justify-center'
+  );
+
+  const bannerImgCls = clsx('h-[888px] w-auto object-cover');
+
+  const rightPaneCls = clsx(
+    'w-1/2 p-12',
+    'flex flex-col items-center justify-center'
+  );
+
+  const logoImgCls = clsx('mx-auto mb-12', 'h-[98px] w-[353px]');
+
   return (
-    <div className="hidden md:flex bg-primary-20 min-h-screen">
-      <div className="bg-normal h-[870px] border my-[32px] rounded-[20px] overflow-hidden flex w-full">
-        <div className="w-1/2 bg-normal p-8 flex flex-col justify-center items-center">
+    <div className={rootCls}>
+      <div className={cardCls}>
+        <div className={leftPaneCls}>
           <Image
             src="/image/Banner2.png"
             alt="Login illustration"
             width={556}
             height={888}
-            className="w-auto h-[888px] object-cover "
+            className={bannerImgCls}
+            priority
           />
         </div>
 
-        <div className="w-1/2 p-12 flex flex-col items-center justify-center">
+        <div className={rightPaneCls}>
           <Image
             src="/image/TitleLogo.png"
             alt="title logo"
-            priority
             width={353}
             height={100}
-            className="w-[353px] h-[98px] mb-12 mx-auto"
+            className={logoImgCls}
+            priority
           />
           <LoginForm />
         </div>
